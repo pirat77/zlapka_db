497e69b65f59f844566cc3ffb678444ae7a90d35
Jasio <mcyanek@gmail.com>
2020-10-21 09:33:04 +0200

    removes redundant code

--- fillPreferences.py
+++ fillPreferences.py
@@ -1,9 +1,8 @@
 import connection
 import dao
     
-@connection.connection_handler
-def fill_preferences_table(cursor):
-    preferencesFile = open("./media/preferencesList.txt", "r")
-    for line in preferencesFile:
-        value = line.replace("\n", "").replace("\r", "")
-        dao.insert_preference(value)
+
+preferencesFile = open("./media/preferencesList.txt", "r")
+for line in preferencesFile:
+    value = line.replace("\n", "").replace("\r", "")
+    dao.insert_preference(value)
